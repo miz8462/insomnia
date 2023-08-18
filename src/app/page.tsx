09ea46a3680d42766736a1c8e6b2700126773b66
mizki <wwr8462@gmail.com>
2023-08-18 21:03:04 +0900

    使っていないimportの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useState } from "react";
 import Form from "./components/Form";
 
 export default function Home() {
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-200">
       <h1>INSOMNIA</h1>
