a5ab2bbf6f554a2f8568fa9556247b25f4018771
mizki <wwr8462@gmail.com>
2023-08-19 11:36:29 +0900

    Headerコンポーネントの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Form from "./components/Form";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-200">
-      <h1>INSOMNIA</h1>
+      <Header />
       <Form />
     </main>
   );
